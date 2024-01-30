826bf050880b8fbd0964512c42f2131a65d4f803
Himanshu Singh Bhandari <himanshubhandari4675@gmail.com>
2024-01-30 15:00:48 +0530

    Solving Board.js  error

--- Frontier.js
+++ Frontier.js
@@ -1,32 +1,34 @@
 class QueueFrontier {
-    constructor(){
-       this.frontier = []
+    constructor() {
+        // console.log('constructing new frontier')
+        this.frontier = []
     }
 
     isEmpty() {
-       return this.frontier.length === 0
+        return this.frontier.length === 0
     }
 
-    add(grid){
-       this.frontier.push(grid)
+    add(grid) {
+        this.frontier.push(grid)
     }
 
     remove() {
-       return this.frontier.shift()
+        return this.frontier.shift()
     }
 
-    contains(grid){
-       return this.frontier.some(elt => {
-           elt.i == grid.i && elt.j == grid.j
-       })
+    contains(grid) {
+        return this.frontier.some(elt => {
+            elt.i == grid.i && elt.j == grid.j
+        })
     }
 }
 
+
 class StackFrontier extends QueueFrontier {
-   constructor(){
-       super()
-   }
-   remove (){
-       return this.frontier.pop()
-   }
+    constructor() {
+        super()
+    }
+    remove() {
+        return this.frontier.pop()
+    }
 }
